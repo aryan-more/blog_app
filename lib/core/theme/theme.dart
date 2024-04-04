851870e3d78e2a1d55b326fbe2dee10b559bc546
aryan-more <morearyan@outlook.com>
2024-04-04 18:21:49 +0530

    first commit

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkTheme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPalette.backgroundColor,
+  );
+}
